37e873332f4dbbc5535a142e191387094f1474da
Chris Ayers <clayers@gmail.com>
2023-11-06 22:40:58 +0000

    more cleanup

--- custom_checks_examples/resource_group/fail/main.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_resource_group" "example" {
-  name     = "rg-bad-name"
-  location = "West Europe"
-  tags = {
-    "CostCenter" = "it"
-  }
-}
--- custom_checks_examples/resource_group/pass/main.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_resource_group" "example" {
-  name     = "rg-app-env-region"
-  location = "West Europe"
-  tags = {
-    "CostCenter" = "it"
-  }
-}
